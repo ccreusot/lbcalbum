2d1df2cd84654b38e56beed8761ccb22fce6ac22
Cedric Creusot <cedric.creusot@gmail.com>
2020-04-24 10:40:02 +0200

    change properties to LiveData

--- presentation/src/androidTest/kotlin/fr/cedriccreusot/presentation/list/viewmodels/AlbumListViewModelTest.kt
+++ presentation/src/androidTest/kotlin/fr/cedriccreusot/presentation/list/viewmodels/AlbumListViewModelTest.kt
@@ -3,12 +3,16 @@ package fr.cedriccreusot.presentation.list.viewmodels
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.nhaarman.mockitokotlin2.*
+import com.nhaarman.mockitokotlin2.given
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyZeroInteractions
 import fr.cedriccreusot.domain.entities.Album
 import fr.cedriccreusot.domain.entities.Track
 import fr.cedriccreusot.domain.usecases.FetchAlbumsException
 import fr.cedriccreusot.domain.usecases.FetchAlbumsUseCase
 import fr.cedriccreusot.presentation.utils.CoroutinesTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,6 +24,7 @@ class AlbumListViewModelTest {
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @get:Rule
+    @ExperimentalCoroutinesApi
     val coroutinesTestRule = CoroutinesTestRule()
 
     @Test
@@ -34,11 +39,11 @@ class AlbumListViewModelTest {
 
         val viewModel = AlbumListViewModel(useCase)
         viewModel.isLoading.observeForever(loadingObserver)
-        viewModel.albums.observeForever(albumListObserver)
         viewModel.error.observeForever(errorObserver)
+        viewModel.albums.observeForever(albumListObserver)
 
         verify(loadingObserver).onChanged(true)
-        verify(loadingObserver, times(2)).onChanged(false)
+        verify(loadingObserver).onChanged(false)
         verify(errorObserver).onChanged(error)
         verify(albumListObserver).onChanged(emptyList())
     }
@@ -66,11 +71,11 @@ class AlbumListViewModelTest {
 
         val viewModel = AlbumListViewModel(useCase)
         viewModel.isLoading.observeForever(loadingObserver)
-        viewModel.albums.observeForever(albumListObserver)
         viewModel.error.observeForever(errorObserver)
+        viewModel.albums.observeForever(albumListObserver)
 
         verify(loadingObserver).onChanged(true)
-        verify(loadingObserver, times(2)).onChanged(false)
+        verify(loadingObserver).onChanged(false)
         verifyZeroInteractions(errorObserver)
         verify(albumListObserver).onChanged(emptyList())
         verify(albumListObserver).onChanged(expectedList)
--- presentation/src/main/kotlin/fr/cedriccreusot/presentation/list/viewmodels/AlbumListViewModel.kt
+++ presentation/src/main/kotlin/fr/cedriccreusot/presentation/list/viewmodels/AlbumListViewModel.kt
@@ -1,5 +1,6 @@
 package fr.cedriccreusot.presentation.list.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,16 +11,18 @@ import kotlinx.coroutines.withContext
 
 class AlbumListViewModel(private val useCase: FetchAlbumsUseCase) : ViewModel() {
 
-    val isLoading by lazy {
-        MutableLiveData(false)
+    val isLoading : LiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
     }
 
-    val error by lazy {
+    val error : LiveData<String> by lazy {
         MutableLiveData<String>()
     }
 
-    val albums by lazy {
+    val albums : LiveData<List<AlbumViewModel>> by lazy {
         fetchAlbums()
+        // We must explicit it because the compiler need it. Else it will raise the error below :
+        // Type inference failed: Not enough information to infer parameter T in constructor MutableLiveData<T : Any!>(p0: T!)
         MutableLiveData<List<AlbumViewModel>>(emptyList())
     }
 
@@ -33,10 +36,14 @@ class AlbumListViewModel(private val useCase: FetchAlbumsUseCase) : ViewModel() 
                         AlbumViewModel(album)
                     })
                 }.exceptionOrNull()?.let {
-                    error.postValue(it.message)
+                    error.postValue(it.message ?: "")
                 }
             }
             isLoading.postValue(false)
         }
+    }
+
+    private fun <T : Any> LiveData<T>.postValue(value: T) {
+        (this as MutableLiveData<T>).postValue(value)
     }
 }
